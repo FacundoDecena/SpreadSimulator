428a9591ccd01946675bb60c8115ab7f09cf90a5
Facundo Decena <facundo.decena@gmail.com>
2020-06-18 16:04:39 -0300

    OMP

--- omp/main.c
+++ omp/main.c
@@ -1,7 +1,7 @@
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <omp.h>
 #include "person.h"
 
 #define FALSE 0
@@ -19,7 +19,7 @@
 #define ELDER_SUSCEPTIBILITY 0.9    // enhancers
 #define RISK_FACTOR 0.15            // enhancers
 #define VACCINES_BOOST 0.005        // enhancers
-#define SIZE 1500                   // Matrix size for rows and columns
+#define SIZE 3                   // Matrix size for rows and columns
 
 // Returns a initialize person
 Person generatePerson();
@@ -53,76 +53,75 @@ int main() {
     Person *lastMatrix;
     init(matrix);
     lastMatrix = matrix;
-    srand(time(0));
+    srandom(time(0));
     Person *neighborhood = (Person *) malloc(sizeof(Person) * 9);
     int indexes[9];
     double start, end;
     double cpu_time_used;
 
-    start = omp_get_wtime();;
+    for(i=0;i<SIZE*SIZE;i++){
+        printf("%d ", matrix[i].state);
+        if ((i+1) % SIZE == 0)
+            printf("\n");
+    }
+    start = omp_get_wtime();
     for (i = 0; i < DAYS; i++) {
-#pragma omp parallel
+#pragma omp parallel private(j) firstprivate(indexes, neighborhood) shared(lastMatrix, matrix, i) default(none)
         {
-#pragma omp for private(j)
-            {
-                for (j = 0; j < SIZE * SIZE; j++) {
-                    neighbors(j, indexes);
-                    neighborhood[0] = lastMatrix[indexes[0]];
-                    neighborhood[1] = lastMatrix[indexes[1]];
-                    neighborhood[2] = lastMatrix[indexes[2]];
-                    neighborhood[3] = lastMatrix[indexes[3]];
-                    neighborhood[4] = lastMatrix[indexes[4]];
-                    neighborhood[5] = lastMatrix[indexes[5]];
-                    neighborhood[6] = lastMatrix[indexes[6]];
-                    neighborhood[7] = lastMatrix[indexes[7]];
-                    neighborhood[8] = lastMatrix[indexes[8]];
-//            if (i == 0||i == DAYS - 1) {
-//                printf("%d ", neighborhood[4].state);
-//                if ((j+1) % SIZE == 0)
-//                    printf("\n");
-//            }
-                    switch (neighborhood[4].state) {
-                        case freeCell:
-                            continue;
-                        case susceptible:
-                            susToSick(neighborhood, &neighborhood[4]);
-                            break;
-                        case sickNoContagion:
-                            neighborhood[4].days++;
-                            noConToCon(&neighborhood[4]);
-                            break;
-                        case sickContagion:
-                            neighborhood[4].days++;
-                            conToAis(&neighborhood[4]);
-                            break;
-                        case isolatedSick:
-                            neighborhood[4].days++;
-                            break;
-                        default:
-                            break;
-                    }
-                    sickToD_or_R(&neighborhood[4]);
-                    matrix[j] = neighborhood[4];
+#pragma omp for
+            for (j = 0; j < SIZE * SIZE; j++) {
+                neighbors(j, indexes);
+                neighborhood[0] = lastMatrix[indexes[0]];
+                neighborhood[1] = lastMatrix[indexes[1]];
+                neighborhood[2] = lastMatrix[indexes[2]];
+                neighborhood[3] = lastMatrix[indexes[3]];
+                neighborhood[4] = lastMatrix[indexes[4]];
+                neighborhood[5] = lastMatrix[indexes[5]];
+                neighborhood[6] = lastMatrix[indexes[6]];
+                neighborhood[7] = lastMatrix[indexes[7]];
+                neighborhood[8] = lastMatrix[indexes[8]];
+                switch (neighborhood[4].state) {
+                    case freeCell:
+                        continue;
+                    case susceptible:
+                        susToSick(neighborhood, &neighborhood[4]);
+                        break;
+                    case sickNoContagion:
+                        neighborhood[4].days++;
+                        noConToCon(&neighborhood[4]);
+                        break;
+                    case sickContagion:
+                        neighborhood[4].days++;
+                        conToAis(&neighborhood[4]);
+                        break;
+                    case isolatedSick:
+                        neighborhood[4].days++;
+                        break;
+                    default:
+                        break;
                 }
+                sickToD_or_R(&neighborhood[4]);
+                matrix[j] = neighborhood[4];
+            }
+            lastMatrix = matrix;
         }
-
     }
-//        if (i == DAYS - 2) {
-//            printf("\n");
-//            printf("\n");
-//        }
-        lastMatrix = matrix;
-    }
-    end =  omp_get_wtime();;
+    end = omp_get_wtime();
     cpu_time_used = (end - start);
-    printf("Tiempo transcurrido: %lf\n\n", cpu_time_used);
 
+    for (int k = 0; k < SIZE * SIZE; k++) {
+        printf("%d ", matrix[k].state);
+        if ((k + 1) % SIZE == 0)
+            printf("\n");
+    }
+    printf("\n");
+    printf("Tiempo transcurrido: %lf\n\n", cpu_time_used);
     return 0;
 }
 
 Person generatePerson() {
     Person p;
-    double ran = (double) rand() / (double) RAND_MAX;
+    double ran = (double) random() / (double) RAND_MAX;
     if (ran > DENSITY) {
         p.state = freeCell;
         return p;
@@ -249,7 +248,7 @@ void neighbors(int index, int *n) {
 
 int susToSick(Person *matrix, Person *p) {
     int i, sickNeighbors = 0;
-    double probContagion = 0.0, ageSusceptibility;
+    double probContagion = 0.0, ageSusceptibility = 0.0;
 
     switch (p->age) {
         case kid:
@@ -286,22 +285,29 @@ int susToSick(Person *matrix, Person *p) {
 }
 
 int noConToCon(Person *p) {
-    if (p->days == 4)
+    if (p->days == 4) {
         p->state = sickContagion;
+        return TRUE;
+    }
+    return FALSE;
 }
 
 int conToAis(Person *p) {
     double rand = (double) random() / (double) RAND_MAX;
     if (p->days == 7) {
-        if (rand > 0.1)
+        if (rand > 0.1) {
             p->state = isolatedSick;
+            return TRUE;
+        }
     }
+    return FALSE;
 }
 
 int sickToD_or_R(Person *p) {
     double rand = (double) random() / (double) RAND_MAX;
     if (p->days == 14) {
-        double probDead;
+        p->days = 0;
+        double probDead = 0.0;
         switch (p->age) {
             case kid:
                 probDead = 0.01;
@@ -314,10 +320,13 @@ int sickToD_or_R(Person *p) {
         }
         if (p->vaccines)
             probDead -= VACCINES_BOOST;
-        if (rand < probDead)
+        if (rand < probDead) {
             p->state = dead;
-        else
+            return TRUE;
+        } else {
             p->state = cured;
-        p->days = 0;
+            return FALSE;
+        }
     }
+    return -1;
 }
